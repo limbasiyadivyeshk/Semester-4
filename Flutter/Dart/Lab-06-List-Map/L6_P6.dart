d1f18360bb3318a55bf629ba90c2589b91056f37
Divyesh Limbasiya <104409676+limbasiyadivyeshk@users.noreply.github.com>
2022-12-30 17:35:12 +0530

    .

---
+++ Flutter/Dart/Lab-06-List-Map/L6_P6.dart
@@ -0,0 +1,15 @@
+import "package:intl/intl.dart";
+
+void main(List<String> args) {
+  // for create dart project = "dart create -t console project_name"
+  // for package add ="flutter pub add intl"
+
+  DateTime now = new DateTime.now();
+  print("======================");
+  print(now);
+  print(DateFormat("dd/MM/yyyy").format(now));
+  print(DateFormat("dd-MM-yyyy").format(now));
+  print(DateFormat("dd-MMM-yyyy").format(now));
+  print(DateFormat("dd-MM-yy").format(now));
+  print(DateFormat("dd MMM yy").format(now));
+}
